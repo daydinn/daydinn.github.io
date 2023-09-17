3ca4603b4657f91e12153957d125f65d63fd76ac
daydinn <daydinn98@gmail.com>
2023-09-17 03:31:24 +0200

    activity icons and some fix

--- src/app/activitys/activitys.component.ts
+++ src/app/activitys/activitys.component.ts
@@ -11,22 +11,23 @@ export class ActivitysComponent implements OnInit {
 
   activitys = [
     
-      { label: "Relaxing", description: "Taking time off to unwind and rejuvenate", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "E-mails", description: "Communicating and staying connected digitally", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false },
-      { label: "Running", description: "Active exercise for cardiovascular health", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "red", points: 1, isHovered: false },
-      { label: "Cleaning", description: "Tidying up and maintaining a neat environment", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false },
-      { label: "Journaling", description: "Documenting thoughts, feelings, and experiences", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Working", description: "Engaging in professional or personal tasks", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false },
-      { label: "Partying", description: "Enjoying social gatherings and celebrations", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Lifting", description: "Strength training and muscle building", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "red", points: 1, isHovered: false },
-      { label: "Movie", description: "Watching films for entertainment or relaxation", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Shopping", description: "Purchasing goods or simply browsing stores", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false },
-      { label: "Dating", description: "Building connections and seeking relationships", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Design", description: "Crafting visuals and creating aesthetics", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false },
-      { label: "Meditating", description: "Seeking inner peace and mindfulness", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Detoxing", description: "Cleansing body and mind from toxins", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "red", points: 1, isHovered: false },
-      { label: "Journaling", description: "Expressing oneself through written words", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "green", points: 1, isHovered: false },
-      { label: "Coding", description: "Building and troubleshooting software", img: "/assets/family32.png", hoveredImg: "/assets/family32_hover.png", color: "blue", points: 1, isHovered: false }
+      { label: "Guitar ", description: "Expressing emotions and creating melodies through the strings", img: "/assets/guitar32.png", hoveredImg: "/assets/guitar32hovered.png", color: "green", points: 1, isHovered: false },
+      { label: "E-mails", description: "Communicating and staying connected digitally", img: "/assets/email32.png", hoveredImg: "/assets/email32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Running", description: "Active exercise for cardiovascular health", img: "/assets/running32.png", hoveredImg: "/assets/running32hovered.png", color: "red", points: 1, isHovered: false },
+      { label: "Cleaning", description: "Tidying up and maintaining a neat environment", img: "/assets/cleaning32.png", hoveredImg: "/assets/cleaning32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Coding", description: "Building and troubleshooting software", img: "/assets/coding32.png", hoveredImg: "/assets/coding32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Working", description: "Engaging in professional or personal tasks", img: "/assets/working32.png", hoveredImg: "/assets/working32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Reading", description: "Diving into stories, acquiring knowledge, and expanding horizons", img: "/assets/reading32.png", hoveredImg: "/assets/reading32hovered.png", color: "green", points: 1, isHovered: false },
+      { label: "Lifting", description: "Strength training and muscle building", img: "/assets/lifting32.png", hoveredImg: "/assets/lifting32hovered.png", color: "red", points: 1, isHovered: false },
+      { label: "Movie", description: "Watching films for entertainment or relaxation", img: "/assets/movie32.png", hoveredImg: "/assets/movie32hovered.png", color: "green", points: 1, isHovered: false },
+      { label: "Shopping", description: "Purchasing goods or simply browsing stores", img: "/assets/shopping32.png", hoveredImg: "/assets/shopping32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Dating", description: "Building connections and seeking relationships", img: "/assets/dating32.png", hoveredImg: "/assets/dating32hovered.png", color: "green", points: 1, isHovered: false },
+      { label: "Design", description: "Crafting visuals and creating aesthetics", img: "/assets/design32.png", hoveredImg: "/assets/design32hovered.png", color: "blue", points: 1, isHovered: false },
+      { label: "Pilates", description: "A body conditioning routine to build flexibility and muscle strength", img: "/assets/pilates32.png", hoveredImg: "/assets/pilates32hovered.png", color: "red", points: 1, isHovered: false },
+      { label: "Detoxing", description: "Cleansing body and mind from toxins", img: "/assets/detox32.png", hoveredImg: "/assets/detox32hovered.png", color: "red", points: 1, isHovered: false },
+      { label: "Journaling", description: "Expressing oneself through written words", img: "/assets/journaling32.png", hoveredImg: "/assets/journaling32hovered.png", color: "green", points: 1, isHovered: false },
+      { label: "Meditating", description: "Seeking inner peace and mindfulness", img: "/assets/meditating32.png", hoveredImg: "/assets/meditating32hovered.png", color: "green", points: 1, isHovered: false },
+
   
   
   ];
@@ -55,7 +56,7 @@ export class ActivitysComponent implements OnInit {
   
   
   getBorderStyle(color: string): string {
-    return `2px solid ${color}`; // You can adjust the border style as needed
+    return `2px solid ${color}`; 
   }
 
   toggleHover(activity: Activity) {
