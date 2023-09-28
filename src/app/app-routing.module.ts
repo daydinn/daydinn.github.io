66c0e2ea1acfeb4aa4d27c5669b0f05d5bda5c2c
daydinn <daydinn98@gmail.com>
2023-09-28 02:26:16 +0200

    Reload bug pix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MoodsComponent } from './moods/moods.component';
 import { ActivitysComponent } from './activitys/activitys.component';
 import { UserProgressComponent } from './user-progress/user-progress.component';
 import { StartpageComponent } from './startpage/startpage.component';
+import { HashLocationStrategy,LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: StartpageComponent, pathMatch: 'full' },
@@ -15,7 +16,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], // Hinweis auf { useHash: true }  
   exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule {}
